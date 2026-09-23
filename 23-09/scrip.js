0383fcd474a4cebff4764f3e199bc52820711213
Agatha <agatha.c.freire22@gmail.com>
2026-09-23 11:56:23 -0300

    Aula 23/09 - Estrutura de repetição

---
+++ 23-09/scrip.js
@@ -0,0 +1,193 @@
+//let idade = 21
+
+//if (idade >= 18) {
+ //   console.log("maior de idade")
+//} else {
+ //   console.log("menor de idade")
+
+
+//}
+
+//let media = 7;
+
+//if (media >= 7) {
+   // console.log("Aprovado")
+//} else if(media == 5 || media == 6){
+    //console.log("Recuperação");
+
+//else {
+    //console.log("Reprovado")
+
+
+//}
+    
+//
+
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("5");
+// console.log("6");
+// console.log("7");
+// console.log("8");
+// console.log("9");
+// console.log("10");
+
+//FOR
+//for (variavel;condicao; incremento) {
+    
+    
+    
+//}
+
+//INCREMENTO
+//for (let index = 0; index < 10;index++) {
+//   console.log(index)
+
+//}
+
+//DECREMENTO
+//for (let index = 10; index > 0;index--) {
+  //  console.log(index)
+//}
+
+//INCREMENTO PERSONALIZADO
+//for (let index = 10; index >= 0;index-=2) {
+    //console.log(index)
+//}
+
+//INCREMENTO PERSONALIZADO
+// for (let index = 10; index >= 0;index-=5) {
+//     console.log(index)
+// }
+
+//EXERCÍCIOS
+//Exercicio 01 - Contador
+//Crie um programa que ultilize for para exibir no console os numeros de 1 até 20
+
+//Exercício 02 - Múltiplos
+//Ultilize um for para exibir no console os multiplos de 5 entre 5 e 50
+
+//Exercício 03 - Decremento
+//Ultilize  um for para exibir no console um contagem regressiva de 10 até 0
+//e depois uma mensagem"Contagem finalizada"
+// for (let index = 10; index >= 0; index--) {
+//  console.log(index)
+// }
+
+//console.log("contagem finalizada");
+
+//--------WHILE
+//let contador = 0;
+// while (contador <= 10) {
+
+// console.log(contador);
+   
+// contador+=5;
+
+//}
+
+// //01
+// for (let index = 1; index <=20 ; index++) {
+//     console.log(index)
+    
+// }
+
+// //02
+// for (let index = 5; index <=50; index +=5) {
+//     console.log(index)
+    
+// }
+
+// //03
+// for (let index = 10; index >=0; index--) {
+//     console.log()
+    
+// }
+// console.log("contagem finalizada")
+
+//Exercício 01 - Contagem
+//Ultilize while para exibir no console os números de 10 até 20
+
+//Exercício 02 - Números pares 
+//Ultilize while para exibir os números pares de 2 até 20
+
+//01
+// let contador = 10
+// while (contador <= 20) {
+//     console.log(contador)
+
+// contador++;
+// }
+
+//02
+// let contador = 2
+// while (contador <= 20) {
+//     console.log(contador)
+
+// contador+=2;
+// }
+
+/// --------DO---WHILE
+// let n = 10;
+// while (n < 5) {
+//     console.log("WHILE")
+
+// }
+// do {
+//     console.log("DO---WHILE")
+// } while (n < 5);
+
+// let contador = 0;
+// do {
+//     console.log(contador)
+
+// contador++;
+
+// } while (contador <= 10);
+
+//Exercícios
+//Exercícios 01 - Contagem
+//Ultilize do...while para exibir os numeros de 10 até 100.
+
+//Exercício 02 - Contagem regressiva
+//Ultilize do...while para exibir uma contagem regressiva de 10 até 1
+
+//01
+// let numero = 10;
+// do {
+//     console.log(numero);
+
+// numero++;
+
+// } while (numero <= 100);
+
+//02
+// let contador = 10
+// do {
+//     console.log(contador);
+// contador--;
+// } while (contador >=1);
+
+
+
+//...........ARRAY (LISTA)...............
+let nome = "Levi";
+let nome2 = "Duda";
+let nome3 = "Gustavo";
+let nome4 = "Bernardo";
+
+//              0       1        2           3
+let nomes = ["Levi", "Duda", "Gustavo", "Bernardo"];
+
+console.log(nomes);//mostra a lista completa na mesma linha
+
+console.log(nomes[1]);//mostra o item da posição mencionada entre colchetes
+
+nome4 = "Ana";
+nomes[3]= "Ana";//Altere o valor na posição específicada
+
+console.log(nomes.length);//Mostra o tamanho do Array
+
+
